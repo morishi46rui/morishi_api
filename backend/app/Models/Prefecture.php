5ea39f00c4801024e3d53e10902807a848055b25
morishi46rui <rui46vissel@gmail.com>
2025-06-22 23:20:49 +0900

    feat: 🎸 Prefuctureモデルを実装

--- backend/app/Models/Prefecture.php
+++ backend/app/Models/Prefecture.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use OpenApi\Attributes as OA;
 
 #[OA\Schema(
@@ -13,9 +14,18 @@ use OpenApi\Attributes as OA;
     description: 'Prefecture',
     properties: [
         new OA\Property(property: 'id', type: 'string', description: 'ID（Sqid）', example: 'DzP3tWWwjsIT'),
+        new OA\Property(property: 'name', type: 'string', description: '都道府県名', example: '兵庫県'),
+        new OA\Property(property: 'code', type: 'string', description: '都道府県コード', example: '28'),
     ]
 )]
 class Prefecture extends BaseModel
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['code', 'name'];
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
 }
